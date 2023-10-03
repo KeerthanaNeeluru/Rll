ed7af0cc5c5b80ddfbf5bcbf679aaed103ac5656
keerthana <keerthananeeluru2001@gmail.com>
2023-10-03 18:34:46 +0530

    adding search pipe

--- User/src/app/app-routing.module.ts
+++ User/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductUserComponent } from './components/product-user/product-user.component';
+import { ProductAdminComponent } from './components/product-admin/product-admin.component';
+import { UserDisplayComponent } from './components/user-display/user-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"product-user", component: ProductUserComponent},
+  {path:"product-admin", component: ProductAdminComponent},
+  {path:"user-details", component: UserDisplayComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
